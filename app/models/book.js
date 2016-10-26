3bd055a12d417dcbeff3df04de455b80de44027e
aimen khakwani and ayana powell <aimen@>
2016-10-26 10:54:01 -0700

    set up initial files with basic css for navigation

---
+++ app/models/book.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  bookTitle: DS.attr(),
+  author: DS.attr(),
+  rating: DS.attr(),
+  image: DS.attr(),
+  buyLink: DS.attr(),
+  postTitle: DS.attr(),
+  postAuthor: DS.attr(),
+  postDate: DS.attr(),
+  post: DS.attr()
+});
