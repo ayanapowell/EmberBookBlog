b82a22b3e12a408ea1f581c43aae40766d7f19e0
aimen khakwani and ayana powell <aimen+ayana@>
2016-10-26 13:22:32 -0700

    Add a admin route

---
+++ app/routes/admin.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('book');
+  },
+  actions: {
+    update(book, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          book.set(key, params[key]);
+        }
+      });
+      book.save();
+      this.transitionTo('admin');
+    },
+    savePost(params) {
+      var newPost = this.store.createRecord('book', params);
+      newPost.save();
+      this.transitionTo('admin');
+    },
+    destroyPost(book) {
+      book.destroyRecord();
+      this.transitionTo('admin');
+    }
+  }
+});
